3749e0ecd5b70c1b41d11008f695d680506f2f5e
Andrea Del Carpio <ajdelcarpiogom@email.wm.edu>
2021-10-15 16:48:29 -0400

    Adding

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -26,7 +26,7 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
-CREATE TABLE Dept_Emp(
+CREATE TABLE dept_emp(
 	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
